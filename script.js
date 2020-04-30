554e16807390768ca47d66292f33d536583ca267
Berta Matu <info.berta@gmail.com>
2020-04-30 22:10:29 +0300

    update_home_about

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // video background pause button
-var video = document.getElementById("bckgVideo");
-var btn = document.getElementById("bckgVideoBtn");
-function pauseFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML;
-  } else {
-    video.pause();
-    btn.innerHTML;
-  }
-};
+// var video = document.getElementById("bckgVideo");
+// var btn = document.getElementById("bckgVideoBtn");
+// function pauseFunction() {
+//   if (video.paused) {
+//     video.play();
+//     btn.innerHTML;
+//   } else {
+//     video.pause();
+//     btn.innerHTML;
+//   }
+// };
 
